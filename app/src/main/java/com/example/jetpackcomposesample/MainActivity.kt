fc65d4d904bb538170fe9fa4c5eb78e8813167bd
Sarwar <mostafasarwar23@gmail.com>
2022-05-08 19:21:41 +0600

    create first column and inside of that, first surface with modifier and color properties

--- app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposesample/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.jetpackcomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 import com.example.jetpackcomposesample.ui.theme.Typography
 
@@ -20,7 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeSampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting("Android")
                     CustomText(name = "Robin")
                 }
@@ -35,22 +38,37 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun CustomText(name: String){
+fun CustomText(name: String) {
     Text(
         style = Typography.h5,
         text = name
     )
-
-
 }
+
+/*@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeSampleTheme {
+        Column{
+            Greeting("Android")
+            CustomText("Fantastic")
+        }
+    }
+}*/
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeSampleTheme {
-        Column() {
-            Greeting("Android")
-            CustomText("Fantastic")
+        Column (modifier = Modifier.fillMaxSize()){
+            Surface(
+                modifier =
+                Modifier.height(50.dp)
+                    .width(200.dp),
+                color = MaterialTheme.colors.primary
+            ) {
+
+            }
         }
     }
 }
